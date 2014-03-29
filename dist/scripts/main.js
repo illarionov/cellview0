ed20f5443c74f040bb10d1b013b1efb344172d1c
Alexey Illarionov <littlesavage@rambler.ru>
2014-03-29 20:56:15 +0400

    initial commit

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+!function(t,e,o){"use strict";var a=o.map("map",{center:[52.5377,13.3958],zoom:4});new o.tileLayer("http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png",{minZoom:0,maxZoom:18,attribution:'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'}).addTo(a)}(window,document,L);
